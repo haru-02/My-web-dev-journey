c3cfd2ed26dcdd562793cdc3045edf6aefc411f7
Rahul Balaji <56230164+haru-02@users.noreply.github.com>
2021-04-04 22:53:00 +0400

    using api practice

---
+++ web dev bootcamp/tv_show_search_api_practice/app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#search-form');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const searchTerm = this.elements.query.value;
+    const config = { params: {q: searchTerm} };
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+});
+
+const makeImages= (shows)=> {
+    for(let result of shows)
+    {   
+        if(result.show.image)
+        {
+            const image = document.createElement('img');
+            image.src = result.show.image.medium;
+            document.body.append(image);
+        }
+    }
+
+};

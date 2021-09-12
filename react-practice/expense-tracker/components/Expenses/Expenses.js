8e4acfd3a2a3cce22de2d395a0a41ca182b9fec7
Rahul Balaji <56230164+haru-02@users.noreply.github.com>
2021-09-12 23:23:53 +0400

    Added expense tracker project - learning react.js

---
+++ react-practice/expense-tracker/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const onFilterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChange={onFilterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
